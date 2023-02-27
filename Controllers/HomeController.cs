dfd95b4a47c3f0ebff141c20a57f1dc6e5870729
Venu Gopal CHEPPALI <venuc.in@mouritech.com>
2023-02-27 12:27:52 +0530

    new repo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using PostAsyncWebApi_Test.Models;
+using System.Web.Mvc;
+
+namespace PostAsyncWebApi_Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            PatOBInfo jobject = new PatOBInfo
+            {
+                acokey = 1,
+                practiceid = 1,
+                userid = "1",
+                searchtext = "",
+                option = 1,
+                viewall = 1
+            };
+
+            ViewData["list"] = PostDataModel.PostAsyncWebApi_Test(jobject);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
